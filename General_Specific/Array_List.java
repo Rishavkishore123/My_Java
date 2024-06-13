36c79ae5f78ef05280f0b6a76655301ee0ba2f72
RISHAV KISHORE <rishavkishore732@gmail.com>
2024-06-14 00:19:05 +0530

    learning java_8

---
+++ General_Specific/Array_List.java
@@ -0,0 +1,16 @@
+package General_Specific;
+
+import java.util.*;
+
+class List{
+    
+}
+
+
+
+public class Array_List {
+    public static void main(String[] args) {
+        ArrayList <Integer>list= new ArrayList<>();
+
+    }
+}
