bc855074661ead2d0e83be22311ce68d78339909
RISHAV KISHORE <rishavkishore732@gmail.com>
2024-06-27 23:44:55 +0530

    learning_strings

---
+++ Strings/practiceset1.java
@@ -0,0 +1,13 @@
+package Strings;
+
+public class practiceset1 {
+    public static void main(String[] args) {
+        String str="Rishav";
+        
+        for(int i=0;i<str.length();i++){
+            char m=str.charAt(i);
+            System.out.println(m);
+
+        }
+    }
+}
